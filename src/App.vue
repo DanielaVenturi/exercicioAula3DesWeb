2504d73728adfcf0a7e67f413f2ef644974cdfb6
DanielaVenturi <danielaventuri14@gmail.com>
2023-03-24 11:29:03 -0300

    exercio 2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <script setup>
-import {ref,computed} from "vue";
+import { ref, computed } from "vue";
 const contador1 = ref(0)
-const contador2= ref(0)
-const soma = computed(() => contador1.value + contador2.value) 
+const contador2 = ref(0)
+const soma = computed(() => contador1.value + contador2.value)
 
 </script>
 
 <template>
-<div>
-  Contador 1: {{ contador1 }}
-  <button @click="contador1++">+</button>
-  <button @click="contador1--">-</button>
+  <div class="col-6">
+    Contador 1: {{ contador1 }}
+    <button class="btn btn-primary" @click="contador1++">+</button>
+    <button class="btn btn-primary" @click="contador1--">-</button>
 
-</div>
+  </div>
 
-<div>
-  Contador 2: {{ contador2 }}
-  <button @click="contador2++">+</button>
-  <button @click="contador2--">-</button>
-</div>
-<div>
-  Soma= {{ soma }}
-</div>
-<div v-if="soma > 10" class="verde">È maior que 10</div>
-<div v-else="soma < 10" class="vermelho">é menor que 10</div>
-<div ></div>
-
+  <div>
+    Contador 2: {{ contador2 }}
+    <button class="btn btn-primary" @click="contador2++">+</button>
+    <button class="btn btn-primary" @click="contador2--">-</button>
+  </div>
+  <div>
+    Soma= {{ soma }}
+  </div>
+  <div>A soma é: {{ soma }}</div>
+  <div v-if="soma % 2 === 0">È par</div>
+  <div v-else>È impar</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
